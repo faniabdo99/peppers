0d197e3255c11bf980471625d1d5b4ebe6f292b2
faniabdo99 <faniabdo99@gmail.com>
2021-03-23 00:05:49 +0200

    home

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1,6 +1,11 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use Validator;
+use App\Models\Order;
+use App\Models\Cart;
+use App\Models\Product;
+use App\Models\User;
 class OrderController extends Controller{
     public function getCheckout(){
         return view('orders.checkout');
