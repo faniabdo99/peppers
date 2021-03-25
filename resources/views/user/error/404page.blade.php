6d89315cffdb96232bc93ba58fd41b62a77965d1
Mohamed Mansy <52934889+mohamedmansy316@users.noreply.github.com>
2021-03-25 16:15:13 +0200

    Add Error Pages

--- resources/views/user/error/404page.blade.php
+++ resources/views/user/error/404page.blade.php
@@ -3,20 +3,30 @@
 <body>
     @include('layout.navbar')
 
-    <section class="container p-5 404-page">
-        <img src="{{url('public/images')}}/error/404-pages.jpg" alt="" style="width:inherit">
+    <section class="container p-5 404-page" >
+        <div class="row">
+            <div class="col-lg-12">
+                <img src="{{url('public/images')}}/error/404-pages.jpg" alt="" style="width:inherit">
+
+            </div>
+        </div>
 
     </section>
-    <section class="container">
-            <div class="row">
-            <div class="col-lg-12 d-flex justify-content-between">
-                <a href="{{route('home')}}" target="_blank"> Main Page</a>
-                <a href="{{route('products')}}" target="_blank"> Our Products</a>
-                <a href="{{route('sell.howToSellWithUs')}}" target="_blank"> How To Sell With Us</a>
-                <a href="{{route('static.faqs')}}" target="_blank"> FAQs</a>
-                <a href="{{route('static.whoWeAre')}}" target="_blank"> Who We Are</a>
+    <section class="container" id="404-page">
+        <h2 class="text-center">You Can Go To</h2>
+    <div class="row  justify-content-between">
+            <div class="col-lg-6 col-md-12 d-flex justify-content-between flex-column">
+                <a class="btn btn-warning font-weight-bold mt-3" href="{{route('home')}}" target="_blank"> Main Page</a>
+                <a class="btn btn-warning font-weight-bold mt-3" href="{{route('products')}}" target="_blank"> Our Products</a>
+                <a class="btn btn-warning font-weight-bold mt-3" href="{{route('products')}}" target="_blank"> Contact Us</a>
             </div>
+            <div class="col-lg-6 col-md-12 d-flex justify-content-between flex-column">
+                <a class="btn btn-warning font-weight-bold mt-3" href="{{route('sell.howToSellWithUs')}}" target="_blank"> How To Sell With Us</a>
+                <a class="btn btn-warning font-weight-bold mt-3" href="{{route('static.faqs')}}" target="_blank"> FAQs</a>
+                <a class="btn btn-warning font-weight-bold mt-3" href="{{route('static.whoWeAre')}}" target="_blank"> Who We Are</a>
+
             </div>
+        </div>
     </section>
     @include('layout.footer')
     @include('layout.scripts')
