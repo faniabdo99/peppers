fe398d5c6d032c9db4739f5139934dcb14f61c35
faniabdo99 <faniabdo99@gmail.com>
2021-04-15 16:54:15 +0200

    Latest

--- resources/views/sell/how-to-sell-with-us.blade.php
+++ resources/views/sell/how-to-sell-with-us.blade.php
@@ -23,7 +23,6 @@
                         consign in order to help us determine its value. Also, you will need to upload a couple of
                         photos of the items
                         so we can check its condition.</p>
-
                     <h2 class="static-title">2. SHIP TO US</h2>
                     <p>
                         Within 7-10 working days after submitting your item, you will receive a custom quote with two
@@ -34,27 +33,20 @@
                     <p>This is the fastest way to get cash for your item with payment sent within 10 working days upon
                         receiving your
                         item.</p>
-
                     <h2 class="static-title">CONSIGNMENT ESTIMATE.</h2>
                     <p>This is your estimated payment after we have sold your item (net amount you get in your hands,
                         and not including
                         our consignment service fee, which varies from one item to another). <br>
-
                         After you've reviewed your quote and decide whether you wish to sell your item directly to us or
                         have us consign
                         it for you. The next step is to package your item well, print and attach our CONSIGNMENT
                         AGREEMENT FORM and drop
                         your package off at our nearest store or send it via courier to: <br>
-
                         Pepper’s Luxury Closet <br>
-
                         Karma 1 Mall, Sheikh Zayed City, 6 of October, Giza, Egypt <br>
-
                         PO BOX 12411 <br>
-
                         Email: info@peppersluxury.com <br>
-
-                        Phone: 002-01155436626</p>
+                        Phone: 00201155436626</p>
                     <h2 class="static-title">3. REVIEW OUR OFFER</h2>
                     <p>Once we receive your item, we will inspect it and verify its authenticity*. After that we notify
                         you by email
@@ -74,39 +66,45 @@
                         within two working days. If you chose to consign with us, we will send your payment within two
                         working days after selling your item.</p>
                     <br>
-                    <form action="{{route('sell.postHowToSellWithUs')}}" method="post" enctype="multipart/form-data">
+                    <form class="sell-form" action="{{route('sell.postHowToSellWithUs')}}" method="post" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group">
                             <label for="title">Submit Your Item</label>
                             <input type="text" class="form-control" name="title" id="title" value="{{old('title') ?? '' }}" placeholder="Add item description here eg. Prada Medium Classic Flap Sling Bag" required>
                         </div>
                         <div class="row">
-                            <div class="form-group col-4">
-                                <label for="gender">Select Gender</label>
-                                <select class="form-control" name="gender" id="gender" required>
-                                    <option @if(old('gender') == 'male') selected @endif value="male">Male</option>
-                                    <option @if(old('gender') == 'female') selected @endif value="female">Female</option>
-                                </select>
+                            <div class="col-lg-4 col-12">
+                                <div class="form-group">
+                                    <label for="gender">Select Gender</label>
+                                    <select class="form-control" name="gender" id="gender" required>
+                                        <option @if(old('gender') == 'male') selected @endif value="male">Male</option>
+                                        <option @if(old('gender') == 'female') selected @endif value="female">Female</option>
+                                    </select>
+                                </div>
                             </div>
-                            <div class="form-group col-4">
-                                <label for="category">Select Category</label>
-                                <select class="form-control select-search" name="category" id="category" required>
-                                    @forelse (getCategories() as $Category)
-                                        <option @if(old('category') == $Category->title ) selected @endif value="{{$Category->title}}">{{$Category->title}}</option>
-                                    @empty
-                                        
-                                    @endforelse
-                                </select>
+                            <div class="col-lg-4 col-12">
+                                <div class="form-group">
+                                    <label for="category">Select Category</label>
+                                    <select class="form-control select-search" name="category" id="category" required>
+                                        @forelse (getCategories() as $Category)
+                                            <option @if(old('category') == $Category->title ) selected @endif value="{{$Category->title}}">{{$Category->title}}</option>
+                                        @empty
+                                            
+                                        @endforelse
+                                    </select>
+                                </div>
                             </div>
-                            <div class="form-group col-4">
-                                <label for="brand">Select Brand</label>
-                                <select class="form-control select-search" name="brand" id="brand" required>
-                                    @forelse (getAllBrands() as $Brand)
-                                        <option @if(old('brand') == $Brand->title ) selected @endif value="{{$Brand->title}}">{{$Brand->title}}</option>
-                                    @empty
-                                        
-                                    @endforelse
-                                </select>
+                            <div class="col-lg-4 col-12">
+                                <div class="form-group">
+                                    <label for="brand">Select Brand</label>
+                                    <select class="form-control select-search" name="brand" id="brand" required>
+                                        @forelse (getAllBrands() as $Brand)
+                                            <option @if(old('brand') == $Brand->title ) selected @endif value="{{$Brand->title}}">{{$Brand->title}}</option>
+                                        @empty
+                                            
+                                        @endforelse
+                                    </select>
+                                </div>
                             </div>
                         </div>
                         <div class="row">
