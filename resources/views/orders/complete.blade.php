ed45941ebf07388e028a3e8d4178edffc29ee4eb
faniabdo99 <faniabdo99@gmail.com>
2021-07-11 13:12:09 +0200

    Orders Fix

--- resources/views/orders/complete.blade.php
+++ resources/views/orders/complete.blade.php
@@ -5,12 +5,13 @@
     @include('layout.navbar')
     <div class="container">
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 mt-5 text-center">
                 <h1 class="mt-3">Success!</h1>
                 <p>Your order has been created, a recipt has been emailed to {{$TheOrder->email}}</p>
                 <a href="{{route('home')}}" class="btn btn-brand">Back to Homepage</a>
             </div>
         </div>
     </div>
+    @include('layout.scripts')
 </body>
 </html>
