1256f5857e7d7b6ffc4e5663ff22928eba1428b7
faniabdo99 <faniabdo99@gmail.com>
2021-07-11 13:39:40 +0200

    Orders Fix

--- resources/views/orders/pay.blade.php
+++ resources/views/orders/pay.blade.php
@@ -1,4 +1,4 @@
 @include('layout.header' , [
     'PageTitle' => 'Payment'
 ])
-<iframe src="https://accept.paymob.com/api/acceptance/iframes/154258?payment_token={{$PaymentID}}" style="width:100vw;height:100vh;" frameborder="0"></iframe>
+<iframe src="https://accept.paymob.com/api/acceptance/iframes/13703?payment_token={{$PaymentID}}" style="width:100vw;height:100vh;" frameborder="0"></iframe>
